75979bfb86108dd07953d4eb613c58a87b7fa79e
“Guillermina <guillermina.m.fernandez1987@gmail.com>
2025-11-03 15:24:42 -0300

    Ejercicio 01 - golfScore function

---
+++ JavaScript/ejercicio_01.js
@@ -0,0 +1,22 @@
+const names = [
+    "Hole-in-one!",
+    "Eagle",
+    "Birdie",
+    "Par",
+    "Bogey",
+    "Double Bogey",
+    "Go Home!",
+];
+
+function golfScore(par, strokes) {
+    if (strokes === 1) return names[0];
+    if (strokes <= par - 2) return names[1];
+    if (strokes === par - 1) return names[2];
+    if (strokes === par) return names[3];
+    if (strokes === par + 1) return names[4];
+    if (strokes === par + 2) return names[5];
+    if (strokes === par + 3) return names[6];
+    return 'No existe la combinación.'
+}
+
+console.log(golfScore(5, 4));
