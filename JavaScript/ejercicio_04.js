cbbefe45103d070583e3ee702a650a6a59f6daec
“Guillermina <guillermina.m.fernandez1987@gmail.com>
2025-11-03 19:11:41 -0300

    Ejercicio 04 - titleCase function

--- JavaScript/ejercicio_04.js
+++ JavaScript/ejercicio_04.js
@@ -1,8 +1,7 @@
 function palabraMasLarga(str) {
     const wordsArray = str.split(' ');
     let longestWord = wordsArray[0];
-    for (let i = 1; i < wordsArray.length - 1; i++){
-        console.log(wordsArray[i].length)
+    for (let i = 1; i < wordsArray.length; i++){
         if (wordsArray[i].length > longestWord.length) {
             longestWord = wordsArray[i];
         }
@@ -11,3 +10,16 @@ function palabraMasLarga(str) {
 }
 
 console.log(palabraMasLarga('esta es mi frase, cual es la palabra más larga?'))
+console.assert(palabraMasLarga('Erase una vez que se era') === 'Erase');
+
+
+function titleCase(str) {
+    let wordsArray = str.split(' ');
+    let titleArray = []
+    wordsArray.map(word => {
+        titleArray.push(word.charAt(0).toUpperCase() + word.slice(1));
+    })
+    return titleArray.join(' ');
+}
+
+console.log(titleCase('esta es mi frase, con la primer letra de cada palabra capitalizada.'))
