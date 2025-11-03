9edbe3310ae4f7bc7b2fe750f9c11e31d21e198d
“Guillermina <guillermina.m.fernandez1987@gmail.com>
2025-11-03 20:33:38 -0300

    Ejercicio 09 - addOnlyNums function

---
+++ JavaScript/ejercicio_09.js
@@ -0,0 +1,11 @@
+function addOnlyNums(...rest) {
+    let result = 0
+    rest.forEach(arg => {
+        if (typeof arg === 'number') {
+            result += arg;   
+        }
+    })
+    return result;
+}
+
+console.log(addOnlyNums(1,3,2,5,'esto es', 1, 'prueba'))
