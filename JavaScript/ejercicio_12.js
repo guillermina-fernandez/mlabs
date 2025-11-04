3157b57299b269837518d034bdf144ce6f75fdbe
“Guillermina <guillermina.m.fernandez1987@gmail.com>
2025-11-04 10:57:23 -0300

    Ejercicio 12 - Added difficulty

--- JavaScript/ejercicio_12.js
+++ JavaScript/ejercicio_12.js
@@ -10,3 +10,24 @@ function game(myNum) {
 }
 
 console.log(game(7));
+
+
+function difficultGame() {
+    let randomNum = Math.floor(Math.random() * 10) + 1;
+
+    return function game(myNum) {
+        if (typeof myNum !== 'number' && myNum < 1) return 'Ingrese un número válido.'
+        if (myNum === randomNum) return 'Acertaste. Fin del juego.'
+        if (myNum < randomNum) {
+            randomNum += 1;
+            return 'El número es mayor';
+        } else if (myNum > randomNum) {
+            randomNum += 1;
+            return 'El número es menor';
+        }
+    }
+}
+
+const newGame = difficultGame();
+console.log(newGame(7))
+console.log(newGame(4))
