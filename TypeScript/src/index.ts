b56dbdba804f32030e25de9e2ffab279a866edb1
“Guillermina <guillermina.m.fernandez1987@gmail.com>
2025-11-10 19:08:46 -0300

    Ejercicio 3.3

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -176,3 +176,33 @@ bankAccount.deposit(10, 'Deposit received')
 console.log('[Exercise 3.2]', `Account value: $${bankAccount.money}`);
 
 
+// EJERCICIO 3.3
+
+function computeScore(word: string) {
+    const letters = word.toUpperCase().split('');
+    return letters.reduce((accum, curr) => accum += getPointsFor(curr), 0);
+}
+
+function getPointsFor(letter: string) {
+    const lettersAndPoints: [string, number][] = [
+        ['AEOIULNRST', 1],
+        ['DG', 2],
+        ['BCMP', 3],
+        ['FHVWY', 4],
+        ['K', 5],
+        ['JX', 8],
+        ['QZ', 10],
+    ];
+
+    return lettersAndPoints.reduce((computedScore, pointsTuple) => {
+        const [letters, score] = pointsTuple;
+        if (letters.split('').find((ll) => ll === letter)) {
+            return computedScore += score;
+        }
+        return computedScore;
+    }, 0);
+}
+
+const word = 'zoologico'
+
+console.log('[Ejercicio 3.3]', `${word} vale ${computeScore(word)} puntos.`);
