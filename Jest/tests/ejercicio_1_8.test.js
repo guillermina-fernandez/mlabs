953ef3c3cefc81ec401a336ddaab87caaf6f8adf
“Guillermina <guillermina.m.fernandez1987@gmail.com>
2025-11-14 17:16:26 -0300

    Jest - Ejercicio 1.8

---
+++ Jest/tests/ejercicio_1_8.test.js
@@ -0,0 +1,49 @@
+// setPrice(item: Object, price: Number) => item: Object
+const setPrice = (item, price) => {
+    const newPrice = {
+        name: item.name,
+        price: price
+    }
+    return newPrice;
+};
+
+// addToCart(cart: Array, item: Object) => cart: Array
+const addToCart = (cart, item) => {
+    return [...cart, item];
+};
+
+describe('setPrice()', () => {
+    it('should set the price in the given item object, immutably.', () => {
+        const item = {
+            name: 'test',
+            price: 30,
+        };
+        const copy = Object.assign({}, item);
+
+        const actual = setPrice(item, 50);
+        const expected = {
+            name: 'test',
+            price: 50,
+        };
+
+        expect(actual).toEqual(expected);
+        expect(item).toEqual(copy);
+    });
+});
+
+describe('addToCart()', () => {
+    it('should add item to cart, immutably', () => {
+        const originalCart = [];
+        const item = {
+            name: 'Toy',
+            price: 30,
+        };
+        const copy = originalCart.slice();
+
+        const actual = addToCart(originalCart, item);
+        const expected = [item];
+
+        expect(actual).toEqual(expected);
+        expect(originalCart).toEqual(copy);
+    });
+});
